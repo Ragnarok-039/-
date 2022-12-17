d26e0ef53ac6b3cb6d56ceb4d79ffcfd41efc663
Alexander <brezhnev.aleksandr@gmail.com>
2022-12-17 19:41:28 +0200

    Промежуточный результат ООО Цифромед.

--- ООО Цифромед/main.py
+++ ООО Цифромед/main.py
@@ -1,19 +1,12 @@
 import random
+from test_1 import solve
 
-my_list = [random.randint(1, 100) for _ in range(10)]
-a, b = 2, 6
-target = my_list[a] + my_list[b]
-flag = True
+for _ in range(10):
+    my_list = list(set(random.randint(1, 100) for _ in range(10)))
+    a, b = random.randint(0, len(my_list) - 1), random.randint(0, len(my_list) - 1)
+    target = my_list[a] + my_list[b]
 
-print(my_list)
-print(target)
-
-for i in range(len(my_list)):
-    if flag:
-        a = my_list[i]
-        for j in range(len(my_list)):
-            b = my_list[j]
-            if i != j and a + b == target:
-                print(i, j)
-                flag = False
-                break
+    print(*my_list)
+    print(f'num_1 = {a}, num_2 = {b}')
+    print(f'{my_list[a]} + {my_list[b]} = {target}')
+    print(solve(my_list, target))
--- ООО Цифромед/test_1.py
+++ ООО Цифромед/test_1.py
@@ -1,1 +1,7 @@
 def solve(nums, target):
+    for i in range(len(nums)):
+        a = nums[i]
+        for j in range(len(nums)):
+            b = nums[j]
+            if i != j and a + b == target:
+                return i, j
