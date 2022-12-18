28c95842933804c31e0d1eaeba089899d1a341aa
Alexander <brezhnev.aleksandr@gmail.com>
2022-12-18 12:04:28 +0200

    Добавил функцию is_broken_version().

---
+++ ООО Цифромед/is_broken_version.py
@@ -0,0 +1,10 @@
+def broken_version():
+    import random
+
+    version_dict = {}
+    bad_index = random.randint(0, 9)
+
+    for i in range(10):
+        version_dict.setdefault(i, True if i < bad_index else False)
+
+    print(version_dict)
