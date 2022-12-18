5b62fff727be06aed8de37ea18776a1a3a48d77f
Alexander <brezhnev.aleksandr@gmail.com>
2022-12-18 21:51:18 +0200

    Update test_2.py

--- ООО Цифромед/test_2.py
+++ ООО Цифромед/test_2.py
@@ -1,12 +1,15 @@
 def solve(dict_status):
-    start, end = 0, len(dict_status) - 1
-    while abs(start - end) > 1:
-        mid = (start + end) // 2
-        if dict_status[mid]:
-            start = mid + 1
-        else:
-            end = mid
-    if dict_status[start]:
-        return f'Первая сломанная версия: {end}.'
+    if len(dict_status) == 1 and not dict_status[0]:
+        return 0
     else:
-        return f'Первая сломанная версия: {start}.'
+        start, end = 0, len(dict_status) - 1
+        while abs(start - end) > 1:
+            mid = (start + end) // 2
+            if dict_status[mid]:
+                start = mid + 1
+            else:
+                end = mid
+        if dict_status[start]:
+            return f'Первая сломанная версия: {end}.'
+        else:
+            return f'Первая сломанная версия: {start}.'
