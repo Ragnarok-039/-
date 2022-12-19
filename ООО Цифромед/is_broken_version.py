6b1e17b8f27a22dd62a7b4d2fe2e545418c4abc9
Ragnarok-039 <63465995+Ragnarok-039@users.noreply.github.com>
2022-12-19 09:45:29 +0200

    Комментарии для Цифромед.

--- ООО Цифромед/is_broken_version.py
+++ ООО Цифромед/is_broken_version.py
@@ -1,10 +1,18 @@
+# Функция для генерации словаря версий.
+
+
 def broken_version():
     import random
 
     version_dict = {}
+    # Берем случайный индекс, с которого пойду сломанные версии.
     bad_index = random.randint(0, 9)
 
+    # Формируем словарь из 10 элементов.
+    # Все элементы левее bad_index - True.
+    # Соответственно, все элементы, начиная с bad_index до конца словаря - False.
     for i in range(10):
         version_dict.setdefault(i, True if i < bad_index else False)
 
+    # Возвращает словарь и первый сломанный индекс.
     return version_dict, bad_index
--- ООО Цифромед/main_for_test_2.py
+++ ООО Цифромед/main_for_test_2.py
@@ -1,11 +1,12 @@
 from is_broken_version import broken_version
 from test_2 import solve
 
+# Возьмем для примера 10 случайных событий.
+# for _ in range(10):
 test_list = broken_version()
 bad_index, dict_status = test_list[1], test_list[0]
 
-print(solve(dict_status))
+print(f'Первая сломанная версия: {solve(dict_status)}.')
 print(bad_index)
 print(dict_status, type(dict_status))
-
-# https://blog.skillfactory.ru/glossary/binarnyj-poisk/
+print('-' * 50)
