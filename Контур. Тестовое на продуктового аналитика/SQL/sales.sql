97c60ef8dda359add7226745629bbf1fe9c2c695
Ragnarok-039 <63465995+Ragnarok-039@users.noreply.github.com>
2023-03-26 13:49:10 +0200

    Промежуточный результат

--- Контур. Тестовое на продуктового аналитика/SQL/sales.sql
+++ Контур. Тестовое на продуктового аналитика/SQL/sales.sql
@@ -1,4 +1,4 @@
-select *
+select bc.bID, bc.piID, bc.Cost, b.BDate, b.PayDate, pi.product, pi.piName
 from BillsContent as bc
          left join Bills as b on bc.bID = b.bID
          left join PriceItems as pi on bc.piID = pi.piID
